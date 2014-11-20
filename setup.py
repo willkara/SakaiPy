6a5bd42ce9df91d5a3ce2188e6bd00ff686d40d4
William Karavites <wkaravites@gmail.com>
2014-11-20 13:31:29 -0500

    Setup changes for impl with PyPi

--- setup.py
+++ setup.py
@@ -10,10 +10,31 @@ setup(name='SakaiPy',
       author_email='wkaravites@gmail.com',
       url='https://github.com/willkara/SakaiPy',
       packages=['SakaiPy','SakaiPy.SakaiTools'],
-      requires={
+      install_requires={
       		"mechanize",
       		"cookielib",
       		"requests",
-      		"simplejson"}
+      		"simplejson"},
+      classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Information Technology',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Topic :: Education',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4'
+
+
+      ],
+      keywords='Sakai Education RESTful'
      )
 			
