55cff979819884fa299eaa816839e05ae9a09085
William Karavites <wkaravites@gmail.com>
2014-11-19 19:54:46 -0800

    Changed directory structure for future working setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='SakaiPy',
       author='William Karavites',
       author_email='wkaravites@gmail.com',
       url='https://github.com/willkara/SakaiPy',
-      packages=['SakaiTools'],
+      packages=['SakaiPy'],
       requires={
       		"mechanize",
       		"cookielib",
