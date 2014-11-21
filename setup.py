f28a21a9dc6137e09d8ce5be876c45349bfa8fd0
William Karavites <wkaravites@gmail.com>
2014-11-21 15:06:44 -0500

    New versions number for form func

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='SakaiPy',
-      version='1.0.1',
+      version='1.1.0',
       description='Python interface to the Sakai RESTful API\'s',
       license='MIT',
       author='William Karavites',
