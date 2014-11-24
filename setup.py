bf3ceb149fe6a959ca26b91e4ac7ef8634e9d05a
William Karavites <wkaravites@gmail.com>
2014-11-24 11:09:46 -0500

    Only works on 2.7, take out 3.x for pip

--- setup.py
+++ setup.py
@@ -24,15 +24,7 @@ setup(name='SakaiPy',
             'Operating System :: OS Independent',
             'Topic :: Education',
             'Topic :: Software Development :: Libraries :: Python Modules',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.0',
-            'Programming Language :: Python :: 3.1',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4'
-
-
+            'Programming Language :: Python :: 2.7'
       ],
       keywords='Sakai Education RESTful'
      )
