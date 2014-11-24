41e4973bb1a361dd42e41fdd886effb0730c3550
William Karavites <wkaravites@gmail.com>
2014-11-24 11:11:06 -0500

    Ver change for 2.x deps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='SakaiPy',
-      version='1.1.0',
+      version='1.1.1',
       description='Python interface to the Sakai RESTful API\'s',
       license='MIT',
       author='William Karavites',
