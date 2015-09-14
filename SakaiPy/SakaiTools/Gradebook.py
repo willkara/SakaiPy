f652da181ea4d833db84a7846fddb7fad578ab19
William Karavites <wkaravites@gmail.com>
2015-09-14 16:01:34 -0400

    Currently not in use. Logic incoming.

--- SakaiPy/SakaiTools/Gradebook.py
+++ SakaiPy/SakaiTools/Gradebook.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import SakaiPy.RequestGenerator
 
 
 class Gradebook(object):
+    """
+    Currently not in use.
+    """
 
-    def __init__(self, rq):
-        self.requester = rq
-
-    def getGradebookForSite(self, siteid):
-        return self.requester.executeRequest('/direct/gradebook/site/{0}.json'.format(siteid))
-
-    def getAllMyGrades(self):
-        return self.requester.executeRequest('/direct/gradebook/my.json')
-
-    def getGradeDetails(self, siteid, name):
-        return self.requester.executeRequest('/direct/gradebook/event/{0}/{1}.json'.format(siteid,name))
-
-
-
+    @staticmethod
+    def amIGoingToFail():
+        """
+        Just stop trying
+        :return: Failure, regret and  tears.
+        """
+        return "Probably"
