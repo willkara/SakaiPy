5a7efe88fe306aebee6fd23b01803c228896645c
William Karavites <wkaravites@gmail.com>
2015-09-14 16:07:09 -0400

    I CAN'T COMMIT TO THIS COMMIT.

--- SakaiPy/SakaiTools/WebContent.py
+++ SakaiPy/SakaiTools/WebContent.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import SakaiPy.RequestGenerator
-
 
 
 class WebContent(object):
+    """
+    Contains logic for the Sakai WebContent tool.
 
+    More information about the RESTful interface can be found at:
+    https://sakai.rutgers.edu/webcontent/news/describe
+    """
     def __init__(self, rq):
+        """
+        Create a standalone WebContent Object
+        :param rq: The RequestHandler to use.
+        :return: A WebContent object
+        """
         self.requester = rq
 
-
-    def getWebContentForSite(self,siteid):
+    def getWebContentForSite(self, siteid):
+        """
+        YOU GET A WEBCONTENT, YOU GET A WEBCONTENT, EVERYONE GETS A WEBCONTENT!
+        :param siteid: PIZZA!
+        :return: HOTDOGS!
+        """
         return self.requester.executeRequest('/direct/webcontent/site/{0}.json'.format(siteid))
