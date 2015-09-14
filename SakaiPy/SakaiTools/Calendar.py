d77d55ac2453c3e50268a853a3149090619e56da
William Karavites <wkaravites@gmail.com>
2015-09-14 15:43:40 -0400

    Code cleanup, formatting and documentation fixes.

--- SakaiPy/SakaiTools/Calendar.py
+++ SakaiPy/SakaiTools/Calendar.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import SakaiPy.RequestGenerator
 
 
 class Calendar(object):
+    """
+    Contains logic for the Sakai Calendar tool.
 
+    More information about the RESTful interface can be found at:
+    https://sakai.rutgers.edu/direct/calendar/describe
+    """
     def __init__(self, rq):
+        """
+        Create a standalone Calendar Object
+        :param rq: The RequestHandler to use.
+        :return: A Calendar object
+        """
         self.requester = rq
 
-    def getCalendarForSite(self, siteid):
-        return self.requester.executeRequest('/direct/calendar/site/{0}.json'.format(siteid))
-
     def getAllMyEvents(self):
+        """
+        Get all of the currently available events for the currently logged in user.
+        :return: A JSON representation of the currently available events for the currently logged in user.
+        """
         return self.requester.executeRequest('/direct/calendar/my.json')
 
     def getEventDetails(self, siteid, eventid):
-        return self.requester.executeRequest('/direct/calendar/event/{0}/{1}.json'.format(siteid,eventid))
-
-
-
+        """
+        Get the details for the specified event within the specified site.
+        :param siteid: The ID of the site.
+        :param eventid: The ID of the event.
+        :return: A JSON representation of the specified event within the specified site.
+        """
+        return self.requester.executeRequest('/direct/calendar/event/{0}/{1}.json'.format(siteid, eventid))
